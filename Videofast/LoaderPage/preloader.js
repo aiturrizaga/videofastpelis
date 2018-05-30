0e0bd55075d93869ebc55426cbdf4433331b9fd3
Alvaro Iturrizaga Vargas <aiturrizaga@vallegrande.edu.pe>
2018-05-30 11:30:05 -0500

    Add files via upload

---
+++ Videofast/LoaderPage/preloader.js
@@ -0,0 +1,7 @@
+/*La animacion dejara de girar cuando carge la pagina*/
+window.onload = function(){
+	var contenedor = document.getElementById('contenedor_carga');
+
+	contenedor.style.visibility = 'hidden';
+	contenedor.style.opacity = '0';
+}
